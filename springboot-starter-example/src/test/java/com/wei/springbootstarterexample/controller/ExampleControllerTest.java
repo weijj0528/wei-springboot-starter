4afe08d998b6d7486b594b9c2f493a11de89299f
william <william.wei@g-town.com.cn>
2019-04-11 18:25:21 +0800

    添加测试

---
+++ springboot-starter-example/src/test/java/com/wei/springbootstarterexample/controller/ExampleControllerTest.java
@@ -0,0 +1,50 @@
+package com.wei.springbootstarterexample.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.wei.springbootstarterexample.SpringbootStarterExampleApplicationTests;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class ExampleControllerTest extends SpringbootStarterExampleApplicationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+
+    //方法执行前初始化数据
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    @Test
+    public void hello() {
+        try {
+            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/example/hello"))
+                    .andExpect(MockMvcResultMatchers.status().is(200))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            String result = mvcResult.getResponse().getContentAsString();
+            JSONObject resultObj = JSON.parseObject(result);
+            // 判断接口返回json中success字段是否为true
+            Assert.assertEquals(resultObj.getIntValue("code"), 200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
