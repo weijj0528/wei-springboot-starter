1604c48d9722798606a02ba4755c7d3949717c3c
weijj0528 <weijj0528@163.com>
2019-07-19 18:15:48 +0800

    结果封装调整

--- springboot-starter/src/main/java/com/wei/springboot/starter/bean/ResultBean.java
+++ springboot-starter/src/main/java/com/wei/springboot/starter/bean/ResultBean.java
@@ -1,11 +1,9 @@
 package com.wei.springboot.starter.bean;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.wei.springboot.starter.exception.BaseException;
-import com.wei.springboot.starter.exception.ErrorEnum;
-import com.wei.springboot.starter.utils.ObjUtils;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -15,82 +13,41 @@ import java.io.Serializable;
  * @author
  */
 @Data
-@Deprecated
-public class ResultBean implements Serializable, Cloneable {
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultBean<T> implements Serializable, Cloneable {
     /**
      * @author
      */
     private static final long serialVersionUID = 8632964679110167394L;
     /**
-     * 错误信息
-     */
-    private String msg;
-    /**
      * 错误编号
      */
-    private int code;
+    private int code = 200;
     /**
-     * 业务返回值
+     * 错误信息
      */
-    private JSONObject result;
+    private String msg = "success";
+    /**
+     * 业务数据
+     */
+    private T data;
 
-    public ResultBean() {
-        super();
-        this.msg = ErrorEnum.SUCCESS.getMsg();
-        this.code = ErrorEnum.SUCCESS.getCode();
-        this.result = new JSONObject();
+    public ResultBean(T data) {
+        this.data = data;
     }
 
-    public ResultBean(BaseException exception) {
-        this.msg = exception.getMessage();
-        this.code = exception.getCode();
-        this.result = new JSONObject();
+    public static <T> ResultBean<T> success() {
+        return new ResultBean<>();
     }
 
-    public ResultBean(ErrorEnum msgEnum) {
-        this.msg = msgEnum.getMsg();
-        this.code = msgEnum.getCode();
-        this.result = new JSONObject();
+    public static <T> ResultBean<T> success(T data) {
+        return new ResultBean<>(data);
     }
 
-    public ResultBean(ErrorEnum errorEnum, String msg) {
-        this(errorEnum);
-        this.result = new JSONObject();
-    }
-
-    public ResultBean(Object result) {
-        this.msg = ErrorEnum.SUCCESS.getMsg();
-        this.code = ErrorEnum.SUCCESS.getCode();
-        if (result == null) {
-            this.result = new JSONObject();
-        } else if (ObjUtils.isArray(result)) {
-            JSONObject json = new JSONObject();
-            json.put("list", result);
-            this.result = json;
-        } else if (ObjUtils.isString(result)) {
-            this.msg = String.valueOf(result);
-            this.result = new JSONObject();
-        } else if (ObjUtils.isBoolean(result)) {
-            JSONObject json = new JSONObject();
-            json.put("status", result);
-            this.result = json;
-        } else {
-            if (result.getClass() == JSONObject.class) {
-                this.result = (JSONObject) result;
-            } else {
-                this.result = JSON.parseObject(JSON.toJSONString(result));
-            }
-        }
-    }
-
-
-    @Override
-    public ResultBean clone() {
-        try {
-            return (ResultBean) super.clone();
-        } catch (Exception e) {
-            return null;
-        }
+    public static <T> ResultBean<T> failure(int code, String msg) {
+        return new ResultBean<>(code, msg, null);
     }
 
 }
