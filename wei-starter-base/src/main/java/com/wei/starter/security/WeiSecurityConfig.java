5423a7d9df1f8c3fd8f86c7b239a395511a5e586
Weijj0528 <Weijj0528@163.com>
2024-05-28 20:17:34 +0800

    fix: security

--- wei-starter-base/src/main/java/com/wei/starter/security/WeiSecurityConfig.java
+++ wei-starter-base/src/main/java/com/wei/starter/security/WeiSecurityConfig.java
@@ -6,15 +6,19 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 /**
  * The type Wei security config.
@@ -24,7 +28,8 @@ import java.util.List;
  */
 @Slf4j
 @Configuration
-public class WeiSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WeiSecurityConfig {
 
     @Value("${spring.security.enable:false}")
     private boolean enable;
@@ -32,12 +37,14 @@ public class WeiSecurityConfig extends WebSecurityConfigurerAdapter {
     private List<String> openApis;
     @Value("${spring.security.cors.path-pattern:/**}")
     private String pathPattern;
-    @Value("${spring.security.cors.path-pattern:*}")
+    @Value("${spring.security.cors.origins:*}")
     private String[] origins;
-    @Value("${spring.security.cors.path-pattern:*}")
+    @Value("${spring.security.cors.headers:*}")
     private String[] headers;
-    @Value("${spring.security.cors.path-pattern:*}")
+    @Value("${spring.security.cors.methods:*}")
     private String[] methods;
+
+    private static final String ALL = "*";
 
     /**
      * Token authentication filter wei token filter.
@@ -51,29 +58,34 @@ public class WeiSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfig(){
+    public WebMvcConfigurer corsConfig() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                AtomicBoolean allowCredentials = new AtomicBoolean(true);
+                Optional.ofNullable(origins).map(Stream::of)
+                        .map(s -> s.findFirst().orElse(ALL))
+                        .filter(s -> ALL.equals(s))
+                        .ifPresent(s -> allowCredentials.set(false));
                 registry.addMapping(pathPattern)
                         .allowedOrigins(origins)
                         .allowedHeaders(headers)
                         .allowedMethods(methods)
                         .maxAge(3600)
-                        .allowCredentials(true);
-                log.info("addCorsMappings success!");
+                        .allowCredentials(allowCredentials.get());
+                log.info("addCorsMappings success: {} {} {} {} {}", pathPattern, origins, headers, methods, allowCredentials.get());
             }
         };
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         log.info("SecurityConfig {}", enable);
         // CSRF关闭
         http.csrf().disable().cors();
         if (!enable) {
             http.authorizeRequests().anyRequest().permitAll();
-            return;
+            return http.build();
         }
         http.formLogin().disable()
                 .exceptionHandling()
@@ -104,6 +116,7 @@ public class WeiSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
         // 添加过滤器
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 }
